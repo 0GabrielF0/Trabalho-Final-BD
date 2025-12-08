fb26d86480b7b35681bd2175da44b2da1a0cbd52
Pedro Rebello <166177261+PedroRebello1@users.noreply.github.com>
2025-12-08 19:29:28 -0300

    Shell refatorado para preparar todos os arquivos e rodar com um unico comando

---
+++ start.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+cd ..
+
+echo "Removendo arquivos originais..."
+rm -rf streamlit mongodb
+
+echo "Copiando novos arquivos..."
+cp -r Trabalho-Final-BD/app ./streamlit
+cp -r Trabalho-Final-BD/data ./mongodb
+
+echo "Removendo arquivos de instalação..."
+rm -rf Trabalho-Final-BD
+
+echo "Configurando permissões..."
+cd streamlit
+chmod +x start.sh stop.sh
+
+echo ""
+echo "Instalação concluída com sucesso!"
+
+echo""
+echo "Iniciando ambiente ..."
+
+# Garante que a rede existe
+docker network create mybridge 2>/dev/null || echo "--- Rede 'mybridge' verificada."
+
+# Inicia MongoDB
+echo "Iniciando MongoDB"
+cd ../mongodb
+chmod +x wait-for-it.sh
+docker-compose up -d
+
+
+# Aguarda o MongoDB
+./wait-for-it.sh localhost 27017 echo "MongoDB disponível"
+
+cd ../streamlit
+
+# Inicia Streamlit
+echo "Subindo Dashboard"
+docker-compose up --build -d
+
+echo ""
+echo "Ambiente pronto!"
+echo ""
+echo "Segure Ctrl e Clique no link abaixo:"
+echo "http://localhost:8501"
+echo ""
+echo "Mongo Express: http://localhost:8081"
