7448e4a667b6646e01fda7b600a7c8b33f752382
Pedro Rebello <166177261+PedroRebello1@users.noreply.github.com>
2025-12-08 19:40:07 -0300

    Set executable permissions for start.sh and stop.sh
    Added executable permissions to start.sh and stop.sh.

--- start.sh
+++ start.sh
@@ -11,9 +11,10 @@ echo "Copiando novos arquivos..."
 cp -r Trabalho-Final-BD/app ./streamlit
 cp -r Trabalho-Final-BD/data ./mongodb
 
+chmod +x start.sh stop.sh
+
 echo "Configurando permissões..."
 cd streamlit
-chmod +x start.sh stop.sh
 
 echo ""
 echo "Instalação concluída com sucesso!"
